2fc5f4571d19a79e768051d8f39b2e01d85357b9
Christopher <christopher.ogbunuzor@cloudreach.com>
2021-11-30 19:21:09 +0000

    Completed lambda practice and code works

--- BotoScript.py
+++ BotoScript.py
@@ -1,18 +1,18 @@
 import json
 import boto3
 def handler(event, context):
-    s3_obj =boto3.client('s3') #creating object for accessing s3
+    s3_obj =boto3.client('s3',aws_access_key_id="AKIAS2VFK44PPNK7DOCE",aws_secret_access_key="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") #creating object for accessing s3
 
-    s3__pet_obj = s3_obj.get_object(Bucket='my-first-lambdabucket-194694014750', Key='PetDb.json')
-    s3__pet_data = s3__pet_obj['Body'].read().decode('utf-8')
+    s3_pet_obj = s3_obj.get_object(Bucket='my-first-lambdabucket-194694014750', Key='PetDb.json')
+    s3__pet_data = s3_pet_obj['Body'].read().decode('utf-8')
 
 
     s3_pet_list=json.loads(s3__pet_data)
     print("json loaded data")
     
 
-    lo_pet_dict=next(item for item in s3_pet_list if item["name"] == event.PetName)
-    print(lo_pet_dict)
+    lo_pet_dict=next(item for item in s3_pet_list['pets'] if item["name"] == event["PetName"])
+    
 
     return lo_pet_dict.get('favFoods')
     
--- iam-lambda.tf
+++ iam-lambda.tf
@@ -7,6 +7,6 @@ resource "aws_iam_role_policy" "lambda_policy" {
 #Create an EXECUTION role with an attached policy 
 #for labmda FUNCTION
 resource "aws_iam_role" "lambda_role" {
-  name               = "lambda_role"
+  name = "lambda_role"
   assume_role_policy = file("IAM/lambda_assume_role_policy.json")
 }
